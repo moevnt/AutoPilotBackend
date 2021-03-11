c04659747db8f68aa3013edbe1a8761e0dc85c45
moevnt <evan.monteith@gmail.com>
2021-03-11 13:28:15 -0500

    Algorithm progress

--- Backend/DataRetrival.py
+++ Backend/DataRetrival.py
@@ -1,23 +1,60 @@
 from pandas_datareader import data as wb
 import datetime
+import statistics as stats
 
 today = datetime.date.today().isoformat()
-previous20 = datetime.date.today() - datetime.timedelta(days=20)
+previous20 = datetime.date.today() - datetime.timedelta(days=40)
+print(previous20)
+
 
 def getTickerInfo(ticker):
-	return wb.DataReader(ticker, data_source='yahoo', start=previous20, end=today)
+	return wb.DataReader(ticker, data_source='yahoo', start=previous20, end='2021-03-10')
 
+
+# return wb.DataReader(ticker, data_source='yahoo', start=previous20, end='2021-03-10')
 
 class DataRetrieval:
 	pass
 	
-	def calculateBands(self, ticker):
+	def getTP(self, ticker):
+		date_frame = getTickerInfo(ticker)
+		TP = {}
+		for i in range(20, 0, -1):
+			high = date_frame.iloc[i][0]
+			low = date_frame.iloc[i][1]
+			close = date_frame.iloc[i][5]
+			tp = (high + low + close) / 3.0
+			TP[i] = tp
+			print(TP[i])
+			
+	
+	def calculateMovingAverage(self, ticker):
 		date_frame = getTickerInfo(ticker)
 		# Get moving average = MA(TP,n) where TP = (high+low+close)/3 && n = number of days
-		MA = {}
-		for i in 20:
-			TP = 0
-	# 2 * standard deviation of MA
+		sum = 0.0
+		TP = self.getTP(self, ticker=ticker)
+		TP.sort()
+		for i in range(0, 20):
+			sum += TP[i]
+		
+		MA = sum / 20
+		return MA
+	
+# 2 * standard deviation of MA
+	def calculateUpperBand(self, ticker):
+		ma = self.calculateMovingAverage(ticker)
+		upper = ma + (2*stats.stdev(self.getTP(ticker)))
+		return upper
+		
+	def calculateLowerBand(self, ticker):
+		ma = self.calculateMovingAverage(ticker)
+		lower = ma - (2*stats.stdev(self.getTP(ticker)))
+		return lower
 
-df = getTickerInfo('AAPL')
-print(df.head())
+df = getTickerInfo('GOOGL')
+#print(df.iloc[20])
+
+data = DataRetrieval
+ma = data.calculateMovingAverage(data, ticker='AAPL')
+print(ma)
+#data.getTP(data, 'AAPL')
--- Backend/Decision.py
+++ Backend/Decision.py
@@ -1,9 +1,13 @@
 from Backend import DataRetrival, Trading
+from pandas_datareader import data as wb
+import datetime
 
 retrieval = DataRetrival
 trade = Trading
+today = datetime.date
+print(today)
 
 class Decision:
 	
 	def makeDecision(self, ticker):
-		retrieval.getTickerInfo()
+		wb.DataReader(ticker, data_source='yahoo', start=today)
